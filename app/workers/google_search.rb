10f2d132cbaf294532bd3e500d9e6f9ecace9daa
dszromik <dominik.szromik@polcode.net>
2015-03-16 16:22:52 +0100

    security update
    security update part 2

--- app/workers/google_search.rb
+++ app/workers/google_search.rb
@@ -11,14 +11,14 @@ class GoogleSearch
     search_form.q = query_term
     page = agent.submit(search_form)
 
-    query = Query.where(name: query_term.downcase).first_or_create(user_id: user_id)
+    query = Query.where("name = ?",  query_term.downcase).first_or_create(name: query_term.downcase, user_id: user_id)
 
     page.search('.g').each do |p|
       a_html_tag = p.at('a').to_s
       next if a_html_tag.include? 'images'
       clean_name = strip(a_html_tag)
       clean_url = a_html_tag.split(%r{=|&})
-      Link.where(url: clean_url[2]).first_or_create(name: clean_name, short_description: strip(p.css('.st').to_s), query_id: query.id)
+      Link.where("url = ?", clean_url[2]).first_or_create(name: clean_name, url: clean_url[2],short_description: strip(p.css('.st').to_s), query_id: query.id)
     end
   end
 
