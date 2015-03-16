8277d0017b6832f73729f1a73c62713bf61392ae
dszromik <dominik.szromik@polcode.net>
2015-03-16 12:59:43 +0100

    add short_description to link

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312155120) do
+ActiveRecord::Schema.define(version: 20150316115811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,9 +19,10 @@ ActiveRecord::Schema.define(version: 20150312155120) do
   create_table "links", force: :cascade do |t|
     t.string   "name"
     t.string   "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.integer  "query_id"
+    t.string   "short_description"
   end
 
   create_table "queries", force: :cascade do |t|
