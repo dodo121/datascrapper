b37e7204861ec7c63762feb597600d48d6ed89c5
dszromik <dominik.szromik@polcode.net>
2015-03-17 13:30:05 +0100

    add visitors view

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,5 +1,5 @@
 class QueriesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :index
 
   expose(:queries)
   expose(:query, attributes: :query_params)
